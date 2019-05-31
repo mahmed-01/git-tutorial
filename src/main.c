ffca3502a15b3190ca18cef50dd7d5c6cf83cdee
Mahmoud Ahmed <mahmoud.ahmed@tum.de>
2019-05-31 02:22:32 +0200

    Convert number

--- src/main.c
+++ src/main.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
+#include <espl_lib.h>
+#include <stdbool.h>
 
 int main (void)
 {
-	printf("Hello ESPL!");
-	printf("Added text");
+	char decision;
+	do
+	{
+		printf("Enter a whole number:\n");
+		unsigned int num;
+		scanf("%u", &num);
+		char* dnum = num_to_words(num);
+		printf("The number is %s\n", dnum);
+		printf("Do you want to enter another number? (Y/N)\n");
+		scanf("%*c%c", &decision);
+	}
+	while (decision == 'Y');
+	if (decision != 'N')
+	{
+		printf("Invalid input\n");
+	}				
 }
